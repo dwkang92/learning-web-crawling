36f6eb33ce69be385ec61eda41c3e9d3f73d9321
dwkang92 <dwkang921@gmail.com>
2021-08-11 20:14:51 +0900

    update day2.py

--- day2.py
+++ day2.py
@@ -7,6 +7,7 @@
 # PUT 방식: requests.put()
 # DELETE 방식: requests.delete()
 
+import requests
 
 r = requests.get(
     'http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
